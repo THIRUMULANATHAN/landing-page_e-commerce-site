6a4d5644ba674aa50268cca98fc35df7bc84f052
THIRUMULANATHAN V <148784833+THIRUMULANATHAN@users.noreply.github.com>
2024-02-22 21:12:29 +0530

    Add files via upload

---
+++ shopsphere.js
@@ -0,0 +1,9 @@
+var sidenavbar = document.querySelector(".side-navbar");
+
+function showNavbar() {
+    sidenavbar.style.display = "block";
+}
+
+function closeNavbar() {
+    sidenavbar.style.display = "none";
+}
